7adf36dba96811c6ed111229f6a57a2736991ee5
demoffsrmain <ustimdima2002@gmail.com>
2026-03-08 18:35:21 +0100

    Initial Commit

---
+++ Bound/BoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoundApp.swift
+//  Bound
+//
+//  Created by Dmitry Demidov on 08.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct BoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bound/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bound
+//
+//  Created by Dmitry Demidov on 08.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
